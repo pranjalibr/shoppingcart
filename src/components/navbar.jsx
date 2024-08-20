84aaa58e01201ed2083ccb0b9a18f58d698fa020
Pranjali bramhankar <pranjalibramhankar14@gmail.com>
2024-08-20 10:57:10 +0530

    updated navbar.css

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { ShoppingCart } from 'phosphor-react'
-import './navbar.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { ShoppingCart } from 'phosphor-react';
+import './navbar.css';
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="navbar">
-      <div className="links">
+      <div className="hamburger" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className={`links ${isOpen ? 'active' : ''}`}>
         <Link to="/"> Shop </Link>
         <Link to="/contact"> Contact </Link>
         <Link style={{ marginTop: '8px' }} to="/cart">
@@ -14,5 +25,5 @@ export const Navbar = () => {
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
